65c5a314ffea51738e78d693b2d3705057546133
wjhwang <wjhwang@nglp.kr>
2021-11-15 13:58:46 +0900

    Add utility class for plugins that collect location data

--- fl_location_platform_interface/lib/fl_location_platform_interface.dart
+++ fl_location_platform_interface/lib/fl_location_platform_interface.dart
@@ -13,6 +13,7 @@ export 'package:fl_location_platform_interface/src/models/location.dart';
 export 'package:fl_location_platform_interface/src/models/location_accuracy.dart';
 export 'package:fl_location_platform_interface/src/models/location_permission.dart';
 export 'package:fl_location_platform_interface/src/models/location_services_status.dart';
+export 'package:fl_location_platform_interface/src/utils/location_utils.dart';
 
 part 'src/method_channel_fl_location.dart';
 part 'src/platform_interface_fl_location.dart';
---
+++ fl_location_platform_interface/lib/src/utils/location_utils.dart
@@ -0,0 +1,48 @@
+import 'dart:math';
+
+import 'package:vector_math/vector_math.dart';
+
+/// Utility class for plugins that collect location data.
+class LocationUtils {
+  /// Calculate the distance between origin and destination.
+  static double distanceBetween(
+    double startLat,
+    double startLon,
+    double endLat,
+    double endLon,
+  ) {
+    final dLat = _toRadians(endLat - startLat);
+    final dLon = _toRadians(endLon - startLon);
+
+    final a = pow(sin(dLat / 2), 2) +
+        pow(sin(dLon / 2), 2) *
+            cos(_toRadians(startLat)) *
+            cos(_toRadians(endLat));
+    final c = 2 * asin(sqrt(a));
+
+    return 6378137.0 * c;
+  }
+
+  /// Calculate the bearing between origin and destination.
+  static double bearingBetween(
+    double startLat,
+    double startLon,
+    double endLat,
+    double endLon,
+  ) {
+    final startLatRadians = radians(startLat);
+    final startLonRadians = radians(startLon);
+    final endLatRadians = radians(endLat);
+    final endLonRadians = radians(endLon);
+
+    var y = sin(endLonRadians - startLonRadians) * cos(endLatRadians);
+    var x = cos(startLatRadians) * sin(endLatRadians) -
+        sin(startLatRadians) *
+            cos(endLatRadians) *
+            cos(endLonRadians - startLonRadians);
+
+    return degrees(atan2(y, x));
+  }
+
+  static _toRadians(double degree) => degree * pi / 180;
+}
