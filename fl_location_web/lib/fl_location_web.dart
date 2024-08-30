e7421a43b1945ebe788b507b468d78b1a0569e71
wjhwang <hwj930513@naver.com>
2024-08-30 14:16:23 +0900

    chore(fl_location_web): Upgrade dependencies

--- fl_location_web/lib/fl_location_web.dart
+++ fl_location_web/lib/fl_location_web.dart
@@ -1,6 +1,8 @@
 import 'dart:html' as html;
+import 'dart:developer' as dev;
 
 import 'package:fl_location_platform_interface/fl_location_platform_interface.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 class FlLocationWeb extends FlLocationPlatform {
@@ -19,9 +21,13 @@ class FlLocationWeb extends FlLocationPlatform {
 
   @override
   Future<LocationPermission> checkLocationPermission() async {
-    final permissionStatus = await _permissions?.query({'name': 'geolocation'});
+    // ref: https://developer.chrome.com/blog/permissions-api-for-the-web
+    final html.PermissionStatus? permissionStatus =
+        await _permissions?.query({'name': 'geolocation'});
 
-    switch (permissionStatus?.state) {
+    final String? permissionState = permissionStatus?.state;
+
+    switch (permissionState) {
       case 'granted':
         return LocationPermission.whileInUse;
       case 'prompt':
@@ -29,17 +35,19 @@ class FlLocationWeb extends FlLocationPlatform {
       case 'denied':
         return LocationPermission.deniedForever;
       default:
-        throw ArgumentError(
-            'Unknown permission status ${permissionStatus?.state}');
+        dev.log('location permission denied: $permissionState');
+        return LocationPermission.denied;
     }
   }
 
   @override
   Future<LocationPermission> requestLocationPermission() async {
     try {
+      // show permission prompt
       await _geolocation.getCurrentPosition();
       return LocationPermission.whileInUse;
-    } catch (_) {
+    } catch (e) {
+      dev.log('location permission denied: $e');
       return LocationPermission.deniedForever;
     }
   }
@@ -49,12 +57,12 @@ class FlLocationWeb extends FlLocationPlatform {
     LocationAccuracy accuracy = LocationAccuracy.best,
     Duration? timeLimit,
   }) async {
-    final position = await _geolocation.getCurrentPosition(
+    final html.Geoposition position = await _geolocation.getCurrentPosition(
       enableHighAccuracy: accuracy.index >= LocationAccuracy.high.index,
       timeout: timeLimit,
     );
 
-    return _toLocation(position);
+    return toLocation(position);
   }
 
   @override
@@ -65,12 +73,12 @@ class FlLocationWeb extends FlLocationPlatform {
   }) {
     return _geolocation
         .watchPosition(
-          enableHighAccuracy: accuracy.index >= LocationAccuracy.high.index,
-        )
-        .map(_toLocation);
+            enableHighAccuracy: accuracy.index >= LocationAccuracy.high.index)
+        .map(toLocation);
   }
 
-  Location _toLocation(html.Geoposition position) {
+  @visibleForTesting
+  Location toLocation(html.Geoposition position) {
     return Location.fromJson({
       'latitude': position.coords?.latitude,
       'longitude': position.coords?.longitude,
@@ -79,9 +87,7 @@ class FlLocationWeb extends FlLocationPlatform {
       'heading': position.coords?.heading,
       'speed': position.coords?.speed,
       'speedAccuracy': 0.0,
-      'millisecondsSinceEpoch': position.timestamp != null
-          ? DateTime.fromMillisecondsSinceEpoch(position.timestamp!)
-          : DateTime.now(),
+      'millisecondsSinceEpoch': position.timestamp,
       'isMock': false,
     });
   }
